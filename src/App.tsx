403beb09f9e38fff5e03a0e4fa2cf3b5822b3cce
Victor Bittencourt <victorgcb@outlook.com>
2024-10-11 10:32:42 -0300

    merge: something

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+
+  return (
+    <ChakraProvider>
+      <h1>Olá</h1>
+    </ChakraProvider>
+  )
+}
+
+export default App
